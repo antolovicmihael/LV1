46a1f7ddff8f93a5136e29fcb7ce6bc72a510b21
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-04-24 17:35:41 +0200

    DOOP Zad 2 i 3

--- Lv1/Lv1/Program.cs
+++ Lv1/Lv1/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Lv1
+namespace Zad2i3
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Note note = new Note();
+            Note note1 = new Note("Note", "Mihael", 1);
+            Note note2 = new Note("Note2", "Miho");
+            note2.setImportance(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(note.ToString());
+            Console.WriteLine(note1.ToString());
+            Console.WriteLine(note2.ToString());
         }
     }
 }
