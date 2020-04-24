063f38c126fde754cc416bff1ef1fceb82ace8c7
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-04-24 17:54:18 +0200

    DOOP Zadaci 4 i 5

--- Lv1/Lv1/Program.cs
+++ Lv1/Lv1/Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿//using System;
 
-namespace Zad2i3
-{
-    class Program
-    {
+//namespace Zad2i3
+//{
+//    class Program
+//    {
         
-        static void Main(string[] args)
-        {
-            Note note = new Note();
-            Note note1 = new Note("Note", "Mihael", 1);
-            Note note2 = new Note("Note2", "Miho");
-            note2.setImportance(Convert.ToInt32(Console.ReadLine()));
-            Console.WriteLine(note.ToString());
-            Console.WriteLine(note1.ToString());
-            Console.WriteLine(note2.ToString());
-        }
-    }
-}
+//        static void Main(string[] args)
+//        {
+//            Note note = new Note();
+//            Note note1 = new Note("Note", "Mihael", 1);
+//            Note note2 = new Note("Note2", "Miho");
+//            note2.setImportance(Convert.ToInt32(Console.ReadLine()));
+//            Console.WriteLine(note.ToString());
+//            Console.WriteLine(note1.ToString());
+//            Console.WriteLine(note2.ToString());
+//        }
+//    }
+//}
---
+++ Lv1/Zad4i5/Note.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zad4i5
+{
+    class Note
+    {
+        public String text;
+        private String author;
+        private int NoteImportance;
+
+        public string Text
+        {
+            get { return this.text; }
+            set { this.text = value; }
+        }
+        public string Author
+        {
+            get { return this.author; }
+            set { this.author = value; }
+        }
+        public int Importance
+        {
+            get { return this.NoteImportance; }
+            set { this.NoteImportance = value; }
+        }
+        public string getText() { return this.text; }
+        public string getAuthor() { return this.author; }
+        public int getImportance() { return this.NoteImportance; }
+        public void setText(string text) { this.text = text; }
+        public void setImportance(int importance) { this.NoteImportance = importance; }
+        
+        public Note()
+        {
+            this.text = string.Empty;
+            this.author = string.Empty;
+            this.NoteImportance = 0;
+        }
+        public Note(string text, string author,int importance)
+        {
+            this.text = text;
+            this.author = author;
+            this.NoteImportance = importance;
+        }
+        public Note(string text, string author)
+        {
+            this.text = text;
+            this.author = author;
+        }
+        public override string ToString()
+        {
+            return this.text + this.author+ this.NoteImportance;
+        }
+    }
+}
