5b238696c45fb7e310e3185c73c1ef1915e661a4
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-04-24 18:18:42 +0200

    DOOP Zadatak 6

---
+++ Lv1/Zad6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note note = new Note();
+            Note note1 = new Note("Note", "Mihael", 1);
+            Note note2 = new Note("Note2", "Miho");
+            note2.setImportance(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(note.ToString());
+            Console.WriteLine(note1.ToString());
+            Console.WriteLine(note2.ToString());
+            Console.WriteLine(note.Author);
+            Console.WriteLine(note.Importance);
+            Console.WriteLine(note.Text);
+            Console.WriteLine(note1.Author);
+            Console.WriteLine(note1.Importance);
+            Console.WriteLine(note1.Text);
+            Console.WriteLine(note2.Author);
+            Console.WriteLine(note2.Importance);
+            Console.WriteLine(note2.Text);
+            Note time = new TimeNote();
+            Console.WriteLine(time.ToString());
+        }
+    }
+}
---
+++ Lv1/Zad6/TimeNote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zad6
+{
+    class TimeNote : Note
+    {
+        DateTime NoteTime;
+        public TimeNote() : base()
+        {
+            this.NoteTime = DateTime.Now;
+
+        }
+        public TimeNote(string text, string author, int importance) : base(text, author, importance)
+        {
+            this.NoteTime = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + NoteTime.ToString();
+        }
+    }
+}
